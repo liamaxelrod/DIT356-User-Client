a3e8eedccee6af4dca81c1d7a5469887fc6c0ec2
carlottahillger <78755376+carlottahillger@users.noreply.github.com>
2022-12-22 12:15:17 +0100

    Change to different calendar library

--- Client/src/views/Calendar.vue
+++ Client/src/views/Calendar.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
-    <div> {{message}} </div>
-    <full-calendar class="test-fc" :events="events"
-      first-day='1' locale="fr"
-      @eventClick="eventClick"
-      @moreClick="moreClick">
-        <template slot="fc-event-card" slot-scope="p">
-            <p><i class="fa">sadfsd</i> {{ p.event.title }} test</p>
+  <div class="row">
+    <div class="col-9">
+      <FullCalendar :options="calendarOptions">
+        <template v-slot:eventContent='arg' class="event">
+          <b>{{ arg.timeText }}</b>
+          <i>{{ arg.event.title }}</i>
         </template>
-    </full-calendar>
+      </FullCalendar>
+    </div>
+    <div class="col-3"> {{message}} </div>
   </div>
 </template>
 
 <script>
-import FullCalendar from 'vue-fullcalendar'
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
 
 export default {
   name: 'Calendar',
@@ -22,23 +25,45 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          title: 'Dental Clinic Vasaplatsen',
-          start: '2022-12-09',
-          cssClass: 'blue',
-          data: { Time: '12:00', Address: 'Vasagatan 1' }
-        },
-        {
-          title: 'event2',
-          start: '2022-12-20',
-          end: '2022-12-23',
-          cssClass: ['blue']
+      calendarOptions: {
+        plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        selectable: true,
+        selectMirror: true,
+        dayMaxEvents: true,
+        weekends: false,
+        select: this.handleDateSelect,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        events: [
+          {
+            title: 'Dental Clinic Vasaplatsen',
+            start: '2022-12-09T12:00:00'
+          },
+          {
+            title: 'event2',
+            start: '2022-12-20'
+          }
+        ],
+        eventBackgroundColor: '#0092CA',
+        eventTextColor: 'white',
+        eventTimeFormat: { // like '14:30:00'
+          hour: '2-digit',
+          minute: '2-digit',
+          meridiem: false
         }
-      ]
+      }
     }
   },
   methods: {
+    handleDateClick: function (arg) {
+      console.log('hello')
+      console.log(arg.dateStr)
+    },
+    handleEventClick: function (arg) {
+      console.log(arg)
+    },
     'eventClick'(event, jsEvent, pos) {
       console.log('eventClick', event, jsEvent, pos)
     },
@@ -55,39 +80,74 @@ export default {
   watch: {
     message: function (newVal, oldVal) {
       console.log('this is a trigger function')
+      console.log(this.message)
     }
   }
 }
 </script>
 
 <style>
-.red {
-  background: rgb(235, 77, 77) !important;
-  color: whitesmoke !important;
+a {
+  color: #0092CA;
 }
-.blue {
-  background: #0092CA !important;
-  color: whitesmoke !important;
+
+b { /* used for event dates/times */
+  margin-right: 3px;
 }
-.orange {
-  background: orange !important;
-  color: white !important;
+.demo-app {
+  display: flex;
+  min-height: 100%;
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+  color: #0092CA;
 }
-.green {
-  background: rgb(49, 155, 49) !important;
-  color: white !important;
+.demo-app-sidebar {
+  width: 300px;
+  line-height: 1.5;
+  background: #eaf9ff;
+  border-right: 1px solid #d3e2e8;
 }
-.blue,
-.orange,
-.red,
-.green {
-  font-size: 13px;
-  font-weight: 500;
-  text-transform: capitalize;
+.demo-app-sidebar-section {
+  padding: 2em;
 }
-.event-item {
-  padding: 1px 0 1px 2px !important;
-  size: 200% !important;
-  margin-top: 10px;
+.demo-app-main {
+  flex-grow: 1;
+  padding: 3em;
 }
+.fc { /* the calendar root */
+  --first-color: #0092CA;
+  max-width: 1100px;
+  margin: 0 auto;
+}
+
+.fc-event {
+  height: 120%;
+  color: #0092CA;
+}
+.fc-today-button {
+  background-color: #0092CA;
+}
+
+.fc-button-primary {
+  background-color: #0092CA;
+}
+
+.fc-button {
+  background-color: #0092CA;
+}
+
+:root {
+  --first-color: #0092CA;
+  --second-color: #ff7;
+  --fc-first-color: #0092CA;
+}
+
+.fc-col-header-cell-cushion {
+  color: #0092CA;
+}
+
+.fc-daygrid-day-number {
+  color: #0092CA;
+}
+
 </style>
