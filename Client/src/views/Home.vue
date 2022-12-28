f86c1cf9508b04e24b31a1783c8c8307e20187d8
Juan García Díaz <juandz2000@gmail.com>
2022-12-28 10:40:54 +0000

    Add initial home page version
    - Add text and images
    Co-Authored-By: Carlotta Hillger <hillger@student.chalmers.se>

--- Client/src/views/Home.vue
+++ Client/src/views/Home.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="container">
+  <div>
+    <div class="row">
+      <div class="col">
+        <h1>What is Dentistimo?</h1>
+      </div>
+      <div class="col">
+        <span class="span">
+          Dentistimo is an online service that allows users to make dentist appointments in just a few clicks.
+          Dentistimo gathers all dentist clinics in one place allowing users to quickly scan the entire city of
+          Gothenburg by selecting their preferences and making appointments for a range of services including
+          revisions, dental cleanings and filling cavities. Dentistimo also offers a calendar, where users can
+          check their confirmed appointments and, if necessary, cancel them.
+        </span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="@/assets/gothenburg.jpeg" alt="logo" width="100%" />
+      </div>
+      <div class="col">
+        <img src="@/assets/pug_toothbrush.jpeg" alt="logo" width="100%" />
+      </div>
+      <div class="col">
+        <img src="@/assets/dentist.jpeg" alt="logo" width="100%" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,6 +55,11 @@ export default {
 
 .row {
   padding: 2%;
+}
+
+.span {
+  text-align: justify;
+  display: flex;
 }
 
 @media only screen and (max-width: 768px)  {
